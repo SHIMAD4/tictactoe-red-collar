a3ad4cc5baaf93c66a061e35a26966fc97a6f95e
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-08-08 16:44:33 +0500

    add: Added figure rendering & logic for changing the move

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,40 @@
 // 2. Сделать поле 3х3 клетки, просчитать возможные комбинации для выигрышных ходов игроков
 // 3. По окончанию игры выводить сообщение о победителе
 
+let board = document.querySelector('.game-block')
 let blocks = document.querySelectorAll('.game-block__list-item')
 
+let firstStep = 0
+
 blocks.forEach(block => {
-    block.addEventListener('click', () => {
-        console.log('Hi')
-    })
+    block.addEventListener('click', checkStep)
 })
+
+function checkStep(e) {
+    switch (firstStep) {
+        case 0:
+            createCircle(e.target.getAttribute('id'))
+            firstStep = 1
+            e.target.removeEventListener('click', checkStep)
+            break
+        case 1:
+            createCross(e.target.getAttribute('id'))
+            firstStep = 0
+            e.target.removeEventListener('click', checkStep)
+            break
+    }
+}
+
+function createCircle(id) {
+    let circle = document.createElement('div')
+    circle.classList.add('circle')
+    blocks[id].append(circle)
+    return
+}
+
+function createCross(id) {
+    let cross = document.createElement('div')
+    cross.classList.add('cross')
+    blocks[id].append(cross)
+    return
+}
